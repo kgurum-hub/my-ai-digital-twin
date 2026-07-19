72956bfe082f5aad83d4332d9aeb8f10115d80a2
kgurum-hub <awslearnerlearner282@gmail.com>
2026-07-18 19:06:49 -0700

    Update app.py. fixed ChatBot -> Chatbot

--- app.py
+++ app.py
@@ -296,9 +296,9 @@ def respond_ai(message,history):
 #------------------------------------------------
 demo = gr.ChatInterface(fn=respond_ai,
                  title="Karthi's Digital Twin",
-                 chatbot=gr.ChatBot(avatar_images = (None,"karthik_twin.jpg")),
+                 chatbot=gr.Chatbot(avatar_images = (None,"karthik_twin.jpg")),
                  description="Chat with an AI version of Karthik Gurumurthy. Ask about his experience, projects or just say hi!",
-                 examples="Whats your background?,What kind of projects have you worked on?"
+                 examples="Whats your background?,What kind of projects have you worked on?,"di you have ay other interests or hobbies"
                  )
 if __name__ == "__main__":
     demo.launch(server_name="0.0.0.0", server_port=10000)
